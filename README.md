c102c4f997a460c7e5dbe463d1ac596f59b568e5
Shimakaze9490 <949034746@qq.com>
2018-11-27 10:49:21 +0800

    build the project

---
+++ README.md
@@ -0,0 +1,9 @@
+##nunjucks学习
+1. npm init
+2. add README.md
+3. npm install nunjucks --save
+4. npm install koa --save
+5. koa
+6. views/hello.html
+7. app2.js
+9. 
---
+++ app2.js
@@ -0,0 +1,17 @@
+const Koa = require("koa");
+const app = new Koa();
+const port = 3000;
+const nunjucks = require("nunjucks");
+
+/* 声明：存放模板的目录，views;opts为功能参数 */
+nunjucks.configure("views", { autoescape: true });
+/* Context上下文环境。 */
+app.use(async ctx => {
+    ctx.response.type = "html";
+    // ctx.response.body = "<h1>今天星期二</h1>"
+    // 页面渲染，并传递参数
+    ctx.response.body = nunjucks.render("hello.html", { name: "nunjucks" });
+});
+app.listen(port, res => {
+    console.log("running server at port:"+ port);
+});
